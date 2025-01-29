e0288f3584d3a982b98c4113a294f27de2e8c4c3
Iure Sales <iureguimaraes@gmail.com>
2025-01-29 15:16:24 -0500

    list modified change logs and comments from jira

--- cmd/standup.go
+++ cmd/standup.go
@@ -1,57 +1,156 @@
-/*
-Copyright © 2025 Iure Sales <iureguimaraes@gmail.com>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
 package cmd
 
 import (
 	"fmt"
+	"os"
+	"time"
 
+	jira "github.com/andygrunwald/go-jira"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // standupCmd represents the standup command
 var standupCmd = &cobra.Command{
 	Use:   "standup",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Generate a standup report for the current day",
+	Long: `
+		Generate a standup report for the current day based on activity in the watched repositories:
+		  - Gathers a list of jira tickets assigned to the current user
+			- Checks for status updates on the tickets that happened yesterday
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("standup called")
+		if err := validateConfig(); err != nil {
+			fmt.Println(err)
+
+			os.Exit(1)
+		}
+
+		jiraUsername := viper.GetString("jira.username")
+		jiraToken := viper.GetString("jira.token")
+
+		tp := jira.BasicAuthTransport{
+			Username: jiraUsername,
+			Password: jiraToken,
+		}
+
+		jiraURL := viper.GetString("jira.url")
+		
+		client, err := jira.NewClient(tp.Client(), jiraURL)
+		if err != nil {
+			fmt.Println("Error creating Jira client:", err)
+			return
+		}
+	
+		projectId := viper.GetString("jira.project")
+
+		searchString := fmt.Sprintf(`
+			assignee = currentUser()
+			AND project = %s
+			AND status != Closed
+			AND sprint IN openSprints()
+			AND updated > -1d
+		`, projectId)
+
+		issues, err := getAllIssues(client, searchString)
+		if err != nil {
+			fmt.Println("Error getting issues:", err)
+			return
+		}
+
+		if len(issues) == 0 {
+			fmt.Println("No issues found")
+			return
+		} else {
+			fmt.Printf("Found %d issues\n\n\n", len(issues))
+		}
+
+		// Print all issues's description
+		for _, issue := range issues {
+			fmt.Println("\n\n--------------------------------")
+			fmt.Printf("Issue: %s\n", issue.Key)
+			fmt.Printf("\tSummary: %s\n", issue.Fields.Summary)
+			fmt.Printf("\tStatus: %s\n", issue.Fields.Status.Name)
+
+			yesterday := time.Now().AddDate(0, 0, -1)
+
+			if issue.Fields.Comments != nil {
+				fmt.Println("\tComments:")
+				for _, comment := range issue.Fields.Comments.Comments {
+					fmt.Printf(
+						"\t\t%v - %v: \n %s\n\n",
+						comment.Created,
+						comment.Author.DisplayName,
+						comment.Body,
+					)
+				}
+			}
+
+			for _, changelogHistory := range issue.Changelog.Histories {
+				if changelogHistory.Created > yesterday.Format(time.RFC3339) {
+					for _, changelogItem := range changelogHistory.Items {
+						fmt.Println("\tChange Log:")
+						fmt.Printf("\t\tCreated: %s\n", changelogHistory.Created)
+						fmt.Printf("\t\tAuthor: %s\n", changelogHistory.Author.DisplayName)
+						fmt.Printf("\t\tField: %s\n", changelogItem.Field)
+						fmt.Printf("\t\tFrom String: %s\n", changelogItem.FromString)
+						fmt.Printf("\t\tTo String: %s\n", changelogItem.ToString)
+					}
+				}
+			}
+		}
 	},
+}
+
+func getAllIssues(client *jira.Client, searchString string) ([]jira.Issue, error) {
+	opt := &jira.SearchOptions{
+		MaxResults: 100,
+		Expand:     "changelog",
+		Fields:     []string{"summary", "description", "status", "changelog", "comment"},
+	}
+
+	issues, resp, err := client.Issue.Search(searchString, opt)
+
+	fmt.Printf("Response: %v\n", resp)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return issues, nil
+}
+
+func validateConfig() error {
+	required := []string{"jira.username", "jira.token", "jira.url"}
+	for _, key := range required {
+			if viper.GetString(key) == "" {
+					return fmt.Errorf("missing required config: %s", key)
+			}
+	}
+	return nil
 }
 
 func init() {
 	rootCmd.AddCommand(standupCmd)
 
-	// Here you will define your flags and configuration settings.
+	// Set default values
+	viper.SetDefault("jira.url", "https://ltvco.atlassian.net")
+	
+	// Add flags that can override config file settings
+	standupCmd.Flags().String("jira-username", "", "Jira username (email)")
+	standupCmd.Flags().String("jira-token", "", "Jira API token")
+	standupCmd.Flags().String("jira-url", "", "Jira instance URL")
+	standupCmd.Flags().String("jira-project", "", "Jira project ID")
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// standupCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// Bind flags to viper
+	viper.BindPFlag("jira.username", standupCmd.Flags().Lookup("jira-username"))
+	viper.BindPFlag("jira.token", standupCmd.Flags().Lookup("jira-token"))
+	viper.BindPFlag("jira.url", standupCmd.Flags().Lookup("jira-url"))
+	viper.BindPFlag("jira.project", standupCmd.Flags().Lookup("jira-project"))
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// standupCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// Also check for environment variables
+	viper.BindEnv("jira.token", "JIRA_API_TOKEN")
+	viper.BindEnv("jira.username", "JIRA_USERNAME")
+	viper.BindEnv("jira.url", "JIRA_URL")
+	viper.BindEnv("jira.project", "JIRA_PROJECT")
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,17 @@ module bakuri
 go 1.21.4
 
 require (
+	github.com/andygrunwald/go-jira v1.16.0 // indirect
+	github.com/fatih/structs v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -18,6 +23,7 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.19.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/trivago/tgo v1.0.7 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
